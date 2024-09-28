017cefeba5d4157bcbc8c49cec9b0606e2ef7388
Alex Maxwell <Alexmaxwell3096@gmail.com>
2024-09-28 11:00:21 +0530

    fix:  table responsive issues

--- src/components/TableComponent.tsx
+++ src/components/TableComponent.tsx
@@ -32,29 +32,37 @@ const TableComponent = ({ datas }: any) => {
 
   const navigate = useNavigate();
   const [isModalOpen, setModalOpen] = useState(false);
-  const [correspondingId, setCorrespondingId] = useState('');
-  const [modalContent, setModalContent] = useState<{ title: string; message: any }>({
-    title: '',
-    message: '',
+  const [correspondingId, setCorrespondingId] = useState("");
+  const [modalContent, setModalContent] = useState<{
+    title: string;
+    message: any;
+  }>({
+    title: "",
+    message: "",
   });
 
   const handleEditClick = (id: string) => {
-    setModalContent({ title: 'Edit Item', message: <FormField id={id} onClose={() => setModalOpen(false)} /> });
+    setModalContent({
+      title: "Edit Item",
+      message: <FormField id={id} onClose={() => setModalOpen(false)} />,
+    });
     setModalOpen(true);
-    setCorrespondingId('');
+    setCorrespondingId("");
   };
 
   const handleDeleteClick = (id: string) => {
-    setModalContent({ title: 'Warning', message: 'Are you sure you want to delete this item?' });
+    setModalContent({
+      title: "Warning",
+      message: "Are you sure you want to delete this item?",
+    });
     setModalOpen(true);
-    setCorrespondingId(id)
+    setCorrespondingId(id);
   };
-
 
   const columns = React.useMemo<ColumnDef<any>[]>(
     () => [
       {
-        header: ({ column }:any) => {
+        header: ({ column }: any) => {
           return (
             <div
               className="sm:rounded-tl-md sm:rounded-bl-md"
@@ -68,7 +76,7 @@ const TableComponent = ({ datas }: any) => {
         },
         accessorKey: "name",
         enableSorting: true,
-        cell: ({ row }:any) => (
+        cell: ({ row }: any) => (
           <span className="text-xs text-[#595959] capitalize">
             {row.getValue("name")}
           </span>
@@ -78,7 +86,7 @@ const TableComponent = ({ datas }: any) => {
         header: "Email",
         accessorKey: "email",
         disableSortBy: true,
-        cell: ({ row }:any) => (
+        cell: ({ row }: any) => (
           <span className="text-xs text-[#595959]">
             {row.getValue("email")}
           </span>
@@ -88,7 +96,7 @@ const TableComponent = ({ datas }: any) => {
         header: "Employee ID",
         accessorKey: "employeeId",
         disableSortBy: true,
-        cell: ({ row }:any) => (
+        cell: ({ row }: any) => (
           <span className="text-xs text-[#595959] ml-4">
             {row.getValue("employeeId")}
           </span>
@@ -98,7 +106,7 @@ const TableComponent = ({ datas }: any) => {
         header: "Mobile Number",
         accessorKey: "mobileNumber",
         disableSortBy: true,
-        cell: ({ row }:any) => (
+        cell: ({ row }: any) => (
           <span className="text-xs text-[#595959] ml-4">
             {row.getValue("mobileNumber")}
           </span>
@@ -108,14 +116,14 @@ const TableComponent = ({ datas }: any) => {
         header: "Job Role",
         accessorKey: "JobRole",
         disableSortBy: true,
-        cell: ({ row }:any) => (
+        cell: ({ row }: any) => (
           <span className="text-xs text-[#595959] ml-4 capitalize">
             {row.getValue("JobRole")}
           </span>
         ),
       },
       {
-        header: ({ column }:any) => {
+        header: ({ column }: any) => {
           return (
             <div
               className="ml-8"
@@ -130,13 +138,16 @@ const TableComponent = ({ datas }: any) => {
         // header: 'Actions',
         accessorKey: "id",
         enableSorting: true,
-        cell: ({ row }:any) => (
-          <div className="flex justify-center mr-8">
-            <FaEdit className="text-lg text-green-700 mr-4 cursor-pointer" onClick={() => handleEditClick(row?.original?.id)} />
-            <MdDelete className="text-lg text-red-500 cursor-pointer" onClick={() => handleDeleteClick(row?.original?.id)} />
-
-
-
+        cell: ({ row }: any) => (
+          <div className="flex justify-center">
+            <FaEdit
+              className="text-lg text-green-700 mr-4 cursor-pointer"
+              onClick={() => handleEditClick(row?.original?.id)}
+            />
+            <MdDelete
+              className="text-lg text-red-500 cursor-pointer"
+              onClick={() => handleDeleteClick(row?.original?.id)}
+            />
           </div>
         ),
       },
@@ -166,58 +177,55 @@ const TableComponent = ({ datas }: any) => {
   });
 
   const handleLogout = () => {
-    localStorage.removeItem('user');
-    navigate('/');
+    localStorage.removeItem("user");
+    navigate("/");
   };
   return (
-    <div className="w-full h-full overflow-hidden overflow-y-auto bg-[#EEF9FF]">
-      <div className=" w-[90%] m-auto lg:p-16 md:p-4 sm:overflow-x-auto xs:overflow-x-auto">
+    <div className="w-full h-full overflow-hidden overflow-y-auto bg-[#EEF9FF] p-4">
+      <div className="container mx-auto lg:px-16 md:px-8 sm:px-4 px-2">
         <div className="flow-root">
-
-          <div className="inline-block min-w-full py-2 align-middle bg-white">
-            <h1 className="lg:text-3xl xs:text-2xl font-bold lg:text-center mt-6">Employee List</h1>
-            <div className="lg:flex  lg:justify-end pr-6 gap-3">
-              <Button onClick={() => navigate('/form')} className="fixed top-8 right-6 sm:flex lg:static lg:justify-end"
-                label="Add New Employee" />
-
-<Button   onClick={handleLogout} className="fixed top-8 right-6 sm:flex lg:static lg:justify-end"
-                label="Logout" />
-
+          <div className="min-w-full py-2 align-middle bg-white">
+            <h1 className="text-center font-bold mt-6 text-lg sm:text-xl md:text-2xl lg:text-3xl">
+              Employee List
+            </h1>
+            <div className="flex flex-col sm:flex-row items-center justify-between gap-3 mt-4 p-5">
+              <Button
+                onClick={() => navigate("/form")}
+                className="w-full sm:w-auto lg:w-auto px-4 py-2 text-base sm:text-sm lg:text-lg"
+                label="Add New Employee"
+              />
+              <Button
+                onClick={handleLogout}
+                className="w-full sm:w-auto lg:w-auto px-4 py-2 text-base sm:text-sm lg:text-lg"
+                label="Logout"
+              />
             </div>
-            <div className='relative sm:mx-0 -mx-4 sm:overflow-x-auto'>
-              <table className="lg:w-full  table md:w-[906px] sm:w-[910px] w-[702px] sm:mt-6 mt-[21px] sm:text-sm text-xs text-left rtl:text-right text-gray-500 dark:text-gray-400">
-
-                <thead className="sm:text-sm text-xs text-gray-1500 font-geistmedium sm:leading-[22px] leading-[8px] bg-light-blue/[70%]">
-
-                  {table.getHeaderGroups().map((headerGroup:any) => (
+            <div className="relative sm:overflow-x-auto overflow-x-scroll p-4">
+              <table className="w-full table-auto min-w-[640px] sm:min-w-[700px] md:min-w-[800px] lg:min-w-[900px] text-left text-gray-500 dark:text-gray-400">
+                <thead className="text-sm lg:text-base font-semibold bg-[#FFF3DA] text-darkblue">
+                  {table.getHeaderGroups().map((headerGroup: any) => (
                     <tr key={headerGroup.id}>
-                      {headerGroup.headers.map((header:any) => {
-                        return (
-                          <th
-                            className="bg-[#FFF3DA] text-darkblue font-bold text-sm p-5"
-                            key={header.id}
-                          >
-                            {header.isPlaceholder
-                              ? null
-                              : flexRender(
+                      {headerGroup.headers.map((header: any) => (
+                        <th key={header.id} className="p-4">
+                          {header.isPlaceholder
+                            ? null
+                            : flexRender(
                                 header.column.columnDef.header,
                                 header.getContext()
                               )}
-                          </th>
-                        );
-                      })}
+                        </th>
+                      ))}
                     </tr>
                   ))}
                 </thead>
-
                 <tbody>
                   {table.getRowModel().rows?.length ? (
-                    table.getRowModel().rows.map((row:any) => (
-                      <tr key={row.id} data-state={row.getIsSelected() && "selected"}>
-                        {row.getVisibleCells().map((cell:any) => (
+                    table.getRowModel().rows.map((row: any) => (
+                      <tr key={row.id}>
+                        {row.getVisibleCells().map((cell: any) => (
                           <td
                             key={cell.id}
-                            className="border-b border-gray-200 dark:border-bg-hover p-4"
+                            className="border-b border-gray-200 p-4 text-sm sm:text-base lg:text-lg"
                           >
                             {flexRender(
                               cell.column.columnDef.cell,
@@ -229,7 +237,10 @@ const TableComponent = ({ datas }: any) => {
                     ))
                   ) : (
                     <tr>
-                      <td colSpan={columns.length} className="h-24 text-center">
+                      <td
+                        colSpan={columns.length}
+                        className="h-24 text-center text-sm lg:text-base"
+                      >
                         No Result
                       </td>
                     </tr>
